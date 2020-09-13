f49b26558c25578877ea83cf5b88610d8dcf91b3
gugucon-hwan <gugucon2018@gmail.com>
2020-09-14 03:36:00 +0900

    진도 나감

---
+++ Chapter01/src/main/java/com/rubypaper/Chapter02Application.java
@@ -0,0 +1,18 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+//@SpringBootApplication
+@SpringBootConfiguration 
+@ComponentScan
+@EnableAutoConfiguration
+public class Chapter02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter02Application.class, args);
+	}
+}
